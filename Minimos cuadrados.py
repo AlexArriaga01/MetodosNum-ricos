3bd92ffb38fcb070142e7b3bf51f8adc06bf339b
AlexArriaga01 <alexpoudc@gmail.com>
2026-04-07 13:03:25 -0600

    Add files via upload

--- Minimos cuadrados.py
+++ Minimos cuadrados.py
@@ -1,17 +1,28 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-Px=[]
-Py=[]
+Px=[0,.5,1,1.1,2,3,3.2,3.5,4,5]
+Py=[.1,-.2,1,1.5,1.9,2,3,5,4.5,5]
 
-def Sxy(Px,Py):
-    Sx=0
-    Sy=0
-    for i in Px:
-        Sx=Sx+i
-        Sy=Sy+i
-    Sx=Sx/len(Px)
-    Sy=Sy/len(Py)
-    return Sx,Sy
+Sx=0
+Sy=0
+for i in Px:
+    Sx=Sx+i
+    Sy=Sy+i
+Sx=Sx/len(Px)
+Sy=Sy/len(Py)
 
-    
+m1,m2=0,0
+for i in range(len(Px)):
+    m1=(Px[i]-Sx)*(Py[i]-Sy)
+    m2=(Px[i]-Sx)**2
+
+m=m1/m2
+b=Sy-m*Sx
+
+X=np.linspace(0,5,10)
+Y=m*X+b
+
+plt.scatter(Px,Py)
+plt.plot(X,Y)
+plt.show()
